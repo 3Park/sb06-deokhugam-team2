f934b229be95b462f05a194be99f49f7332a1060
HOGUN00 <7080rjs@gmail.com>
2025-12-02 19:31:09 +0900

    Feat: 도서 논리 삭제, 물리 삭제 리팩토링
    - `@SoftDelete` 사용하면 조회할때 hibernate가 deleted=true 자동으로 제외하고 조회
    - 메서드로 deleted 상태조회하는 건 수정 필요
    - 이슈번호: #116, #117

--- src/main/java/com/codeit/sb06deokhugamteam2/book/repository/BookRepository.java
+++ src/main/java/com/codeit/sb06deokhugamteam2/book/repository/BookRepository.java
@@ -2,27 +2,33 @@ package com.codeit.sb06deokhugamteam2.book.repository;
 
 import com.codeit.sb06deokhugamteam2.book.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
-import java.time.Instant;
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface BookRepository extends JpaRepository<Book, UUID>, BookRepositoryCustom {
-    @Query("""
-                SELECT COUNT(*)
-                FROM Book b
-                WHERE (LOWER(b.title) LIKE LOWER(CONCAT('%', :keyword, '%')) OR
-                LOWER(b.isbn) LIKE LOWER(CONCAT('%', :keyword, '%')) OR
-                LOWER(b.author) LIKE LOWER(CONCAT('%', :keyword, '%'))) AND
-                b.deleted = false
-            """)
-    long countNotDeletedBooksByKeyword(String keyword);
+//    @Query("""
+//                SELECT COUNT(*)
+//                FROM Book b
+//                WHERE (LOWER(b.title) LIKE LOWER(CONCAT('%', :keyword, '%')) OR
+//                LOWER(b.isbn) LIKE LOWER(CONCAT('%', :keyword, '%')) OR
+//                LOWER(b.author) LIKE LOWER(CONCAT('%', :keyword, '%'))) AND
+//                b.deleted = false
+//            """)
+//    long countNotDeletedBooksByKeyword(String keyword);
+//
+//    long countByDeletedFalse();
+//
+//    Optional<Book> findByIsbnAndDeletedFalse(String isbn);
 
-    long countByDeletedFalse();
+    // 논리삭제하면 영속성 컨텍스트에서 제거하기 위해 clearAutomatically = true 설정
+    @Modifying(clearAutomatically = true)
+    @Query("Delete FROM Book b WHERE b.id = :bookId")
+    void deleteSoftById(UUID bookId);
 
-    List<Book> findAllByCreatedAtAfter(Instant since);
-
-    Optional<Book> findByIsbnAndDeletedFalse(String isbn);
+    @Modifying
+    @Query(value = "DELETE FROM book WHERE id = :bookId AND deleted = true", nativeQuery = true)
+    void deleteHardById(UUID bookId);
 }
